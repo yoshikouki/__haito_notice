de5cc200364b482f9fe87a28afebd7591a36b72f
吉本康貴 <yoshikouki@K2.local>
2020-02-24 20:54:23 +0900

    Fix Seed

---
+++ db/data/tdnet_List_RubyHash.rb
@@ -0,0 +1,46 @@
+# Recent.xmlからHashへ。
+{ "TDnetList" => 
+  { "total_count" => "5", 
+    "condition_desc" => "最新順の適時開示情報一覧", 
+    "items" => 
+    { "item" => 
+      [
+        { "Tdnet" => {  "id" => "680187", 
+                        "pubdate" => "2020-02-17 19:00:00", 
+                        "company_code" => "35280", 
+                        "company_name" => "プロスペクト", 
+                        "title" => "主要株主に関する一部報道について", 
+                        "document_url" => "https://webapi.yanoshin.jp/rd.php?https://www.release.tdnet.info/inbs/140120200217466486.pdf", 
+                        "url_report_type_summary" => nil, 
+                        "url_report_type_fs_consolidated" => nil, 
+                        "url_report_type_fs_non_consolidated" => nil, 
+                        "url_report_type_earnings_forecast" => nil, 
+                        "url_report_type_expected_dividends" => nil, 
+                        "url_xbrl" => nil, 
+                        "markets_string" => "東", 
+                        "update_history" => nil } 
+        }, 
+        { "Tdnet" => { "id" => "680180", 
+                        "pubdate" => "2020-02-17 18:10:00", 
+                        "company_code" => "99820", 
+                        "company_name" => "タキヒヨー", 
+                        "title" => "監査等委員会設置会社への移行に関するお知らせ", 
+                        "document_url" => "https://webapi.yanoshin.jp/rd.php?https://www.release.tdnet.info/inbs/140120200217466072.pdf", 
+                        "url_report_type_summary" => nil, 
+                        "url_report_type_fs_consolidated" => nil, 
+                        "url_report_type_fs_non_consolidated" => nil, 
+                        "url_report_type_earnings_forecast" => nil, 
+                        "url_report_type_expected_dividends" => nil, 
+                        "url_xbrl" => nil, 
+                        "markets_string" => "東名", 
+                        "update_history" => nil 
+                        } 
+        }, 
+        { "Tdnet" => { "id" => "680181", "pubdate" => "2020-02-17 18:10:00", "company_code" => "99820", "company_name" => "タキヒヨー", "title" => "完全子会社の吸収合併（簡易合併）に関するお知らせ", "document_url" => "https://webapi.yanoshin.jp/rd.php?https://www.release.tdnet.info/inbs/140120200217466096.pdf", "url_report_type_summary" => nil, "url_report_type_fs_consolidated" => nil, "url_report_type_fs_non_consolidated" => nil, "url_report_type_earnings_forecast" => nil, "url_report_type_expected_dividends" => nil, "url_xbrl" => nil, "markets_string" => "東名", "update_history" => nil } }, 
+        { "Tdnet" => { "id" => "680182", "pubdate" => "2020-02-17 18:00:00", "company_code" => "36950", "company_name" => "Ｍ－ＧＭＯリサーチ", "title" => "役員の人事に関するお知らせ", "document_url" => "https://webapi.yanoshin.jp/rd.php?https://www.release.tdnet.info/inbs/140120200214465943.pdf", "url_report_type_summary" => nil, "url_report_type_fs_consolidated" => nil, "url_report_type_fs_non_consolidated" => nil, "url_report_type_earnings_forecast" => nil, "url_report_type_expected_dividends" => nil, "url_xbrl" => nil, "markets_string" => "東", "update_history" => nil } }, 
+        { "Tdnet" => { "id" => "680183", "pubdate" => "2020-02-17 18:00:00", "company_code" => "36950", "company_name" => "Ｍ－ＧＭＯリサーチ", "title" => "剰余金の配当に関するお知らせ", "document_url" => "https://webapi.yanoshin.jp/rd.php?https://www.release.tdnet.info/inbs/140120200214465945.pdf", "url_report_type_summary" => nil, "url_report_type_fs_consolidated" => nil, "url_report_type_fs_non_consolidated" => nil, "url_report_type_earnings_forecast" => nil, "url_report_type_expected_dividends" => nil, "url_xbrl" => nil, "markets_string" => "東", "update_history" => nil } }
+      ] 
+    }, 
+    "actions" => { "item" => "recent" } 
+  },
+}

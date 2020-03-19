5f1fe7bd74ba04e75a9324a50c01fb953525c601
吉本康貴 <yoshikouki@K2.local>
2020-03-19 18:21:27 +0900

    Watchボタンを実装。一部Ajaxに対応

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -14,5 +14,8 @@ module HaitoNotice
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end

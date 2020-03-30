d5d432e336c2153f27faaa6224b8ab9874c8b7b0
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-03-31 00:41:45 +0900

    Userモデルのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "UserモデルのRSpecテスト。テストはまだない"
+  let(:user) { described_class.new(params) }
+  let(:params) do
+    { name: 'テスト太郎',
+      email: "example@email.com",
+      password: "password",
+      password_confirmation: "password" }
+  end
+
+  it "Userは有効" do
+    expect(user).to be_valid
+  end
+
+  it "不正なUserインスタンスは保存できない" do
+    params.clear
+    expect(user.save).to be_falsey
+  end
+
+  describe "Userのパラメータが不正な場合" do
+    it "User.nameは必須" do
+      params[:name] = ""
+      expect(user).to be_invalid
+    end
+
+    it "User.nameは50文字以内"
+    it "User.emailは必須" do
+      params[:email] = ""
+      expect(user).to be_invalid
+    end
+
+    it "User.emailは255文字以内"
+    it "User.passwordは必須" do
+      params[:password] = ""
+      expect(user).to be_invalid
+    end
+
+    it "User.password_confirmationは必須" do
+      params[:password_confirmation] = ""
+      expect(user).to be_invalid
+    end
+
+    it "User.password_confirmationとpasswordは一致" do
+      params[:password_confirmation] = "wrongPassword"
+      expect(user).to be_invalid
+    end
+  end
 end
--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  it "ホーム画面のテストです"
+  describe "#home" do
+    context "未ログイン時" do
+      it "ログインリンクが表示"
+    end
+
+    context "ログイン時" do
+      it "マイページリンクが表示"
+      it "ウォッチがなかった場合はウォッチするボタンを表示"
+    end
+  end
 end

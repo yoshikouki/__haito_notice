5f6cdf654173e9c330a150e22dade9b67c105ccc
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-02 15:18:59 +0900

    エラー対応。Companies Viewのタイトルを改善

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
+  FactoryBot.create(:company)
   it '企業のテストデータは作られている' do
-    FactoryBot.create(:company)
     company = Company.first
     expect(company.local_code).to eq 1001
   end
 
   it '企業のテストデータは複数保持している' do
-    FactoryBot.create(:company, :more_companies)
-    expect(Company.count).to eq 51
+    expect(Company.count).to be >= 51
   end
 end
--- spec/requests/companies_request_spec.rb
+++ spec/requests/companies_request_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Companies", type: :request do
-  feature "企業一覧ページ" do
-    it '登録企業が一覧表示される' do
-      create_list(:company, 50)
+  feature "全企業一覧" do
+    scenario 'ページタイトルは表示されている' do
       visit companies_path
-      expect(page).to have_content 'テスト株式会社50'
+      expect(page).to have_title '上場企業一覧 | 配当ノーティス'
     end
 
-    it 'セクター毎の一覧が表示される' do
+    scenario '登録企業が一覧表示される' do
       visit companies_path
-      click_link '情報・通信業'
-      expect(page).to have_content 'セクター別'
+      expect(page).to have_selector \
+        'a[href="/companies/1050"]', text: 'テスト株式会社50'
     end
   end
 end

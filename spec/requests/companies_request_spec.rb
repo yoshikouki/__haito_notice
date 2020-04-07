0a8163ef5306dcf456d03f17eddbc1c7731d8f45
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-07 12:44:04 +0900

    企業一覧テストの一部を削除

--- spec/requests/companies_request_spec.rb
+++ spec/requests/companies_request_spec.rb
@@ -6,12 +6,5 @@ RSpec.describe "Companies", type: :request do
       visit companies_path
       expect(page).to have_title '上場企業一覧 | 配当ノーティス'
     end
-
-    scenario '登録企業が一覧表示される' do
-      Company.import FactoryBot.build_list(:company, 50)
-      visit companies_path
-      expect(page).to have_selector \
-        'a', text: Company.find(10).company_name
-    end
   end
 end

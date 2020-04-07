b0b09d9606d11f07deb3a32c3381e3f46bae86b6
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-07 12:40:48 +0900

    企業一覧のテストを修正

--- spec/requests/companies_request_spec.rb
+++ spec/requests/companies_request_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Companies", type: :request do
-  before do
-    company = FactoryBot.build_list(:company, 50)
-    Company.import company
-  end
-
   feature "全企業一覧" do
     scenario 'ページタイトルは表示されている' do
       visit companies_path
@@ -13,9 +8,10 @@ RSpec.describe "Companies", type: :request do
     end
 
     scenario '登録企業が一覧表示される' do
+      Company.import FactoryBot.build_list(:company, 50)
       visit companies_path
       expect(page).to have_selector \
-        'a[href="/companies/1050"]', text: 'テスト株式会社50'
+        'a', text: Company.find(10).company_name
     end
   end
 end

b0ca2a3ed6d1e237291dd5ec7de75635c6a4bf74
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-13 17:55:47 +0900

    update/ アカウント有効化ステップを復元

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,12 +23,12 @@ class UsersController < ApplicationController
       if @user.save
         # メーラー（SendGrid）の不具合により、有効化ステップを再度一時停止
         # ユーザーを有効化するメールを送信
-        # @user.send_activation_email
+        @user.send_activation_email
         # メッセージを作成
-        # flash[:info] = '登録確認用のメールを送信しました。メールを確認し、アカウントを有効化してください'
-        @user.activate
-        log_in @user
-        flash[:info] = 'ご登録ありがとうございます！'
+        flash[:info] = '登録確認用のメールを送信しました。メールを確認し、アカウントを有効化してください'
+        # @user.activate
+        # log_in @user
+        # flash[:info] = 'ご登録ありがとうございます！'
         format.html { redirect_to root_url }
       else
         flash[:danger] = 'アカウントの作成に失敗しました。'

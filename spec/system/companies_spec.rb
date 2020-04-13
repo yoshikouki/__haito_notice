2e17aa06246b8c81af57d0ee9be98185f279213f
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-13 12:26:09 +0900

    add/ SystemSpec 「企業一覧〜セクター一覧経由〜企業個別」を追加。関係Viewを修正

---
+++ spec/system/companies_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Companies", type: :system do
+  let(:company) { FactoryBot.create(:company) }
+
+  before do
+    driven_by(:rack_test)
+    create_webmock("recent.xml?limit=10", "recent_tds.xml")
+    create_webmock("#{company.local_code}.xml?limit=30", "feed_tds.xml")
+  end
+
+  describe "企業一覧" do
+    it "全企業一覧からセクター一覧経由の企業個別ページまで" do
+      visit root_path
+      # 全企業一覧
+      within("header") { click_on 'companies-link' }
+      expect(page).to have_current_path companies_path
+      # セクター一覧
+      click_on "tsi-code-#{company.tsi_code}"
+      expect(page).to have_current_path sectors_path(company.tsi_code)
+
+      # 企業個別ページ
+      click_on "company-link-#{company.local_code}"
+      expect(page).to \
+        have_current_path company_path(company.local_code)
+      expect(find("#company-name")).to \
+        have_content "フィードテスト株式会社1"
+      # 未ログインの場合はウォッチボタンが表示されない
+      expect(find("#login-to-watch")).to \
+        have_content "ログインしてウォッチ"
+    end
+  end
+end

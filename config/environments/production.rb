96dc81d407c43d0fabf14e6016a66f86fe6f13ff
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-18 23:38:24 +0900

    fix/ 有効化メールのURLが不正なバグを修正
    fix/ Production環境での不正なホスト設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://ht-notice.herokuapp.com'
+  host = 'https://haito-notice.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

588727820a08cad05f728857aae3fb1c9f0ba87d
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-19 12:42:25 +0900

    fix/ Password_resets Controller をリファクタリング

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,14 +1,16 @@
 class PasswordResetsController < ApplicationController
+  before_action :set_user, only: [:create, :edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   # GET /password_resets/new
   def new; end
 
   # POST /password_resets/
   def create
-    @user = User.find_by(email: params[:email])
     if @user
       @user.send_reset_email
       flash[:success] = "ご登録メールアドレスにパスワード再設定のURLを送信しました。"
-      redirect_to login_path
+      redirect_to login_url
     else
       flash[:danger] = "不正なメールアドレスです。"
       render 'new'
@@ -16,36 +18,36 @@ class PasswordResetsController < ApplicationController
   end
 
   # GET /password_reset/:reset_token/edit?email=:email
-  def edit
-    correct_access(params[:id])
-  end
+  def edit; end
 
   # POST /password_resets/:id
   def update
-    return unless correct_access(params[:reset_token])
-
-    @user.update(user_params)
-    if @user.save
+    if @user.update(user_params)
       flash[:success] = "パスワードは再設定されました。"
-      redirect_to login_path
+      redirect_to login_url
     else
       flash[:danger] = "不正な入力値です。"
       render :edit
     end
   end
 
-  def correct_access(reset_token)
-    @user = User.find_by(email: params[:email])
-    if @user&.authenticated?(:reset, reset_token)
-      if @user.reset_sent_at.since(1.hour) < Time.zone.now
+  private
+
+  # before_action #create #edit #update
+  def set_user
+    @user = User.find_by(email: params[:email].downcase)
+  end
+
+  # before_action #edit #update
+  def valid_user
+    if @user&.authenticated?(:reset, params[:id])
+      if @user.reset_sent_at < 1.hour .ago
         flash[:danger] = "パスワード再設定URLの有効期限が切れています。"
-        redirect_to login_path
-      else
-        true
+        redirect_to new_password_reset_url
       end
     else
       flash[:danger] = "不正なアクセスです。"
-      redirect_to login_path
+      redirect_to login_url
     end
   end
 

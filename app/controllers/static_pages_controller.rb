ae5960e5b43eb2ba4fd43e1a67cf6e864d44538a
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-23 15:02:27 +0900

    fix/ TD情報の新規ウィンドウアイコンを統一

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,6 @@
 class StaticPagesController < ApplicationController
 
   def home
-    # TDnetの最新情報を読み込み
-    ticker_symbol = "recent"
-    @recent = get_tds(ticker_symbol, 10 )
-
     # ウォッチリスト登録されている企業のTD情報を取得
     if logged_in?
       @user = current_user
@@ -18,5 +14,8 @@ class StaticPagesController < ApplicationController
         @feed = get_tds(param, 10)
       end
     end
+
+    # TDnetの最新情報を読み込み
+    @recent = get_tds("recent", 10 )
   end
 end

2df6dc0aa58ce687dab88a5e5330872ac853e2ba
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-27 12:05:11 +0900

    fix/ ModelSpec Td がRspec all実行時だけレッドになる不具合を修正
    fix/ FactoryBot の Company を修正
    fix/ 上に合わせてその他のSpecを修正

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,7 +1,20 @@
 FactoryBot.define do
-  factory :company, aliases: [:testcompany] do
-    sequence(:company_name) { |n| "テスト株式会社#{n}" }
-    sequence(:local_code) { |n| n + 1000 }
+  factory :company do
+    company_name { "テスト株式会社1" }
+    local_code { 1001 }
+    market_division { '市場第一部（内国株）' }
+    tsi_code { '5250' }
+    topix_sector_indices { '情報・通信業' }
+    t17_code { '10' }
+    topix_17 { '情報通信・サービスその他' }
+    sc_code { '1' }
+    size_classification { 'TOPIX Core30' }
+    pub_date { '20200228' }
+  end
+
+  factory :companies, class: 'Company' do
+    sequence(:company_name) { |n| "テスト株式会社#{n + 1}" }
+    sequence(:local_code) { |n| n + 1001 }
     market_division { '市場第一部（内国株）' }
     tsi_code { '5250' }
     topix_sector_indices { '情報・通信業' }

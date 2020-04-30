04a48bbdf738f6e07b588a81fe3b85b4e3353aef
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-04-30 17:20:44 +0900

    clean/ 不要なコードを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,6 @@ class ApplicationController < ActionController::Base
   # ログイン機能
   include SessionsHelper
 
-  # ロケール振り分けを全てのアクションで実行
-  around_action :switch_locale
-  
-  # params値のロケールによる振り分け
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
-
   # url_for関係メソッドでロケールを設定するよう上書き
   def default_url_options
     { locale: I18n.locale }

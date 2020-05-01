83841a6d5dffdc9490fbcb94db55621520b5f82d
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-01 18:52:05 +0900

    update/ 企業一覧Viewを日本語翻訳
    対象ファイル：index／ sectors／ _sector_list
    
    併せて
    ・リファクタリング
    ・テストの修正

--- spec/requests/companies_request_spec.rb
+++ spec/requests/companies_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Companies", type: :request do
   feature "全企業一覧" do
     scenario 'ページタイトルは表示されている' do
       visit companies_path
-      expect(page).to have_title '上場企業一覧 | 配当ノーティス'
+      expect(page).to have_title '上場企業一覧（全企業） | 配当ノーティス'
     end
   end
 end

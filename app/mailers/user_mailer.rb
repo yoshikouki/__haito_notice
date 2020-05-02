5bd139ea373d60ea2e67d963aaa215e2c3482f65
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-02 15:34:11 +0900

    update/ パスワード再設定の日本語翻訳・リファクタリング

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -18,6 +18,7 @@ class UserMailer < ApplicationMailer
   #
   def password_reset(user)
     @user = user
+    @url = edit_password_reset_url(@user.reset_token, email: @user.email)
     mail to: user.email, subject: "【配当ノーティス】パスワードの再設定"
   end
 end

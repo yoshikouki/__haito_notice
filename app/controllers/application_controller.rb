b514a1226d3170a9c92cb06660852855a63782f6
K.K <53972292+yoshikouki@users.noreply.github.com>
2020-05-29 10:19:50 +0900

    update/ Bootstrap Class 用のハッシュを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   # 許可するparameterをdeviseに通す
   before_action :configure_permitted_parameters, 
                 if: :devise_controller?
+  before_action :set_var
 
   # 全てのアクションでロケールを読み込む
   around_action :switch_locale
@@ -38,10 +39,38 @@ class ApplicationController < ActionController::Base
       end
     end
 
+    def set_var
+      @bootstrap_class_list = BOOTSTRAP_CLASS_LIST.deep_dup
+    end
+
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
-      devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
+
+  BOOTSTRAP_CLASS_LIST = {
+    page_title:           'h4 mb-5',
+    link_list:            '',
+    link_list_btn:        'btn btn-secondary mb-3',
+    link_list_btn_block:  'btn btn-light btn-block mb-3',
+    link_list_text:       'small mb-2',
+    link_btn_secondary:   'btn btn-secondary mb-3',
+    link_btn_block:       'btn btn-primary btn-block mb-5',
+    link_btn_block_light: 'btn btn-light btn-block mb-5',
+    status_badge:         'badge badge-secondary badge-sm col-4 mr-2',
+    job_badge:            'badge badge-light badge-pill',
+    new_badge:            'badge badge-primary badge-sm',
+    form:                 'my-5',
+    form_group:           'form-group mb-3',
+    form_field:           'form-control',
+    form_submit:          'btn btn-block btn-primary font-weight-bold my-4 py-4',
+    form_submit_small:    'btn btn-light font-weight-bold',
+    form_small_text:      'form-text small text-gray',
+    form_errors:          'mb-5',
+    form_errors_title:    'h5',
+    form_errors_lists:    'list-group',
+    form_errors_list:     'list-group-item list-group-item-dark py-1'
+  }.freeze
 end
